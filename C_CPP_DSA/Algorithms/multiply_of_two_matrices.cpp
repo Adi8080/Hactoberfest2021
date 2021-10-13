4f9a2c9deec49c43a06ebf5bb9274ba27969e00c
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-10-13 17:59:47 +0530

    Create multiply_of_two_matrices.cpp

---
+++ C_CPP_DSA/Algorithms/multiply_of_two_matrices.cpp
@@ -0,0 +1,38 @@
+//Multiplication of a two matrix
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    int m1[n1][n2];
+    int m2[n2][n3];
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n2;j++)
+        cin>>m1[i][j];
+    }
+    for(int i=0;i<n2;i++){
+        for(int j=0;j<n3;j++)
+        cin>>m2[i][j];
+    }
+    //now apply algorithm
+    int ans[n1][n3];
+    //initialise ans matrix with 0
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n3;j++)
+        ans[i][j]=0;
+    }
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n3;j++){
+            for(int k=0;k<n2;k++){
+                ans[i][j]+=m1[i][k]*m2[k][j];
+            }
+        }
+    }
+    //to print
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n3;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
