9fea2228cc7e24a402a97059947cb646d423408c
sphinxbard <anishaj.1728@gmail.com>
2021-10-23 00:47:27 +0530

    done

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,41 +1,42 @@
-#include<iostream>
-#include<vector>
-#include<stdlib.h>
-#include<time.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
 
-void selectionsort(vector <int> v)
+void selectionsort(vector<int> v)
 {
-    vector <int> ::iterator i,j,pos;
+    vector<int>::iterator i, j, pos;
     int small;
-    for(i=v.begin();i!=v.end()-1;i++)
+    for (i = v.begin(); i != v.end() - 1; i++)
     {
-        small=*i;
-        for(j=i+1;j!=v.end();j++)
+        small = *i;
+        for (j = i + 1; j != v.end(); j++)
         {
-            if(*j<small)
+            if (*j < small)
             {
-                small=*j;
-                pos=j;
+                small = *j;
+                pos = j;
             }
         }
-        swap(*i,*pos);
+        swap(*i, *pos);
     }
 
-    cout<<"\nSorted vector using selection sort:\n";
-    for(i=v.begin();i!=v.end();i++)
-        cout<<*i<<"  ";
+    cout << "\nSorted vector using selection sort:\n";
+    for (i = v.begin(); i != v.end(); i++)
+        cout << *i << "  ";
 }
 
 int main()
 {
-    int n=10;
-    vector <int> v;
+    int n = 10;
+    vector<int> v;
     srand(time(0));
-    cout<<"\nUnsorted vector:\n";
-    for(int i=0;i<n;i++)
+    cout << "\nUnsorted vector:\n";
+    for (int i = 0; i < n; i++)
     {
-        v.push_back(1+rand()%100);
-        cout<<v[i];
+        v.push_back(1 + rand() % 100);
+        cout << v[i] << "  ";
     }
     selectionsort(v);
     return 0;
