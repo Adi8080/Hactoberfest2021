e5a8d33a46b6b63f7e723850f50b36f84e36e1f4
Tej Pratap <61930589+iampratapbabu@users.noreply.github.com>
2022-10-24 09:43:15 +0530

    all c++ data structure code with explanation

---
+++ dsa-code/code1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header/demoheader.h"
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+    cout<<"---------program started---------"<<endl;
+    Rectangle r1(4,5);
+    cout<<r1.area()<<endl;
+
+    return 0;
+}
+
